ed84aed3f073666583e75307b115cf599f10dc50
vince1393 <vromani@outlook.com>
2019-12-23 00:02:50 -0500

    basic functionality with API call.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+import { Movie, Rating } from "./obj/queries";
+import ptt from "parse-torrent-title";
+import axios from "axios";
+
+const omdbApi: string = "http://www.omdbapi.com/";
+const mock: string[] = [
+  "War Of The Worlds (2005) DVDRip Xvid   EMU",
+  "The Dark Knight",
+  "Scott Pilgrim vs The World [2010]",
+  "Mr & Mrs Smith",
+  "Southpaw (2015) 1080p x264 DD5.1 EN NL Subs",
+  "Ant-Man 2015 1080p BluRay x264 DTS-JYK",
+  "The.Simpsons.S31E06.WEB.x264-XLF[ettv]",
+  "The Simpsons season 1, episode: Bart the Genius",
+  "Charlie's Angels 2000"
+];
+
+const cleanData = (data: string) => {};
+
+const getMovie = async (title: string, year: string = "") => {
+  const formattedTitle = title.split(" ").join("+");
+
+  return await axios
+    .get(
+      `${omdbApi}?apikey=${process.env.REACT_APP_API_KEY}&t=${formattedTitle}&y=${year}&plot=full`
+    )
+    .then(response => response.data);
+};
 
 const App: React.FC = () => {
+  const [movie, setMovie] = useState<Movie[]>([]);
+  useEffect(() => {
+    mock.forEach(movie => {
+      const parsedMovieData: ParseTorrentTitle.ParserResult = ptt.parse(movie);
+      console.log(
+        `${omdbApi}?apikey=${
+          process.env.REACT_APP_API_KEY
+        }&t=${encodeURIComponent(
+          parsedMovieData.title
+        )}&y=${parsedMovieData.year || ""}&plot=full`
+      );
+      axios
+        .get(
+          `${omdbApi}?apikey=${
+            process.env.REACT_APP_API_KEY
+          }&t=${encodeURIComponent(
+            parsedMovieData.title
+          )}&y=${parsedMovieData.year || ""}&plot=full`
+        )
+        .then(response => response.data)
+        .then(data => setMovie(oldMovies => [...oldMovies, data]));
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {movie.map(movie => (
+        <>
+          <div>{movie?.Title}</div>
+          <div>{movie?.Year}</div>
+          <div>{movie?.imdbRating}</div>
+          <div> {movie?.Type}</div>
+          <div> {movie?.Website}</div>
+          <div> {movie?.Plot}</div>
+          <div> {movie?.Response}</div>
+          <div> {movie?.BoxOffice}</div>
+          <div> {movie?.Ratings.toLocaleString()}</div>
+          <img src={movie?.Poster} alt="" />
+        </>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
