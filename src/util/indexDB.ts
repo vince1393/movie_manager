8efdb5d213990dd3ba85a8848a0608c963365cca
vince1393 <vromani@outlook.com>
2020-01-05 16:46:37 -0500

    added indexDB function, not being used yet

--- src/util/indexDB.ts
+++ src/util/indexDB.ts
@@ -1,1 +1,11 @@
 export const indexDB = "";
+const applicationVersion = 1;
+const applicationName = "movie_managerDB";
+
+export const createDB = () => {
+  let openRequest = indexedDB.open(applicationName, applicationVersion);
+
+  openRequest.onerror = () => {
+    console.error("Error", openRequest.error);
+  };
+};
